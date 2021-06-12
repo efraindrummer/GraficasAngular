68caac20603cad2056b3f1a5b104c5eeb149f637
Efrain May <efrain_may_75@hotmail.com>
2021-06-12 13:12:38 -0500

    info mostrada en la grafica

--- src/app/graficas/pages/dona-http/dona-http.component.ts
+++ src/app/graficas/pages/dona-http/dona-http.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartType } from 'chart.js';
+import { Color, Label, MultiDataSet } from 'ng2-charts';
 import { GraficasService } from '../../services/graficas.service';
 
 @Component({
@@ -9,11 +11,38 @@ import { GraficasService } from '../../services/graficas.service';
 })
 export class DonaHttpComponent implements OnInit {
 
+  // Doughnut
+  public doughnutChartLabels: Label[] = [
+    
+  ];
+  public doughnutChartData: MultiDataSet = [
+    
+  ];
+  public doughnutChartType: ChartType = 'doughnut';
+
+  public colors: Color[] = [
+    {
+      backgroundColor: [
+        '#862CF0',
+        '#4E2DF7',
+        '#344BE0',
+        '#2D83F7',
+        '#2BB6ED'
+      ]
+    }
+  ];
+
   constructor(private graficasService: GraficasService) { }
 
   ngOnInit(): void {
     this.graficasService.getUsuariosRedesSociales().subscribe(data => {
       console.log(data);
+
+      const labels = Object.keys(data);
+      const values = Object.values(data);
+
+      this.doughnutChartLabels = labels;
+      this.doughnutChartData.push(values);
     });
   }
 
